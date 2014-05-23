4504f092bca8c39179ca410593a50d8bb856e8cb
roylanceMichael <roylance.michael@gmail.com>
2014-05-23 13:55:29 -0600

    latest version

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name="rjson", 
+	version="0.71", 
+	author="Mike Roylance",
+	description="easy way to get n number of json objects from a string automatically",
+	author_email="roylance.michael@gmail.com",
+	url="https://github.com/roylanceMichael/RandomLengthJsonLoader")
